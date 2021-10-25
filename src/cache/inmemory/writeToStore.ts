cb924e0a7323392b7a3ad30d77718e62c9589aef
Ben Newman <ben@apollographql.com>
2021-10-25 17:10:45 -0400

    Export normalizeReadFieldOptions rather than makeReadFieldFunction.

--- src/cache/inmemory/writeToStore.ts
+++ src/cache/inmemory/writeToStore.ts
@@ -35,8 +35,8 @@ import { InMemoryCache } from './inMemoryCache';
 import { EntityStore } from './entityStore';
 import { Cache } from '../../core';
 import { canonicalStringify } from './object-canon';
-import { makeReadFieldFunction } from './policies';
-import { ReadFieldFunction, ReadFieldOptions } from '../core/types/common';
+import { normalizeReadFieldOptions } from './policies';
+import { ReadFieldFunction } from '../core/types/common';
 
 export interface WriteContext extends ReadMergeModifyContext {
   readonly written: {
@@ -242,24 +242,28 @@ export class StoreWriter {
       incoming.__typename = typename;
     }
 
-    let lazyReadField: undefined | ReturnType<typeof makeReadFieldFunction>;
     const readField: ReadFieldFunction = function (this: void) {
-      const read = lazyReadField || (
-        lazyReadField = makeReadFieldFunction(policies, incoming, context));
-
-      const options: ReadFieldOptions = read.normalizeOptions(arguments);
+      const options = normalizeReadFieldOptions(
+        arguments,
+        incoming,
+        context.variables,
+      );
 
       if (isReference(options.from)) {
         const info = context.incomingById.get(options.from.__ref);
         if (info) {
-          const result = read({ ...options, from: info.storeObject });
+          const result = policies.readField({
+            ...options,
+            from: info.storeObject
+          }, context);
+
           if (result !== void 0) {
             return result;
           }
         }
       }
 
-      return read(options);
+      return policies.readField(options, context);
     };
 
     const fieldNodeSet = new Set<FieldNode>();
